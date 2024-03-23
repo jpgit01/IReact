c1ce9fb97131dcaf6ec045108fa848fb71f738ca
Juan Pablo M. <pablochileidh@gmail.com>
2024-03-23 10:05:58 -0300

    avances de miapi

--- api-c/src/components/MiApi.jsx
+++ api-c/src/components/MiApi.jsx
@@ -1,10 +1,25 @@
-function MiApi() {
+import { useEffect, useState } from "react"
 
+
+function MiApi() {
+const [count, setCount] = useState(0)
+const [dia, setDia] = useState({})
+
+const getDay = async () => {
+  const resp = await fetch('https://api.victorsanmartin.com/feriados/en.json')
+  const data = await resp.json()
+  setDia(data)
+}
+
+useEffect(() => {
+  getDay()
+},[])
   return (
     <>
       <div>
+        <p>Estado es: {count}</p>
+        <button onClick={ () => setCount(count + 1)}>+1</button>
       </div>
-      <h1>Mi Api</h1>
     </>
   )
 }
