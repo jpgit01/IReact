766845ef3e72671e98f3516990432222443dab25
Juan Pablo M. <pablochileidh@gmail.com>
2024-03-23 12:02:50 -0300

    avances para traer datos

--- api-c/src/components/MiApi.jsx
+++ api-c/src/components/MiApi.jsx
@@ -1,27 +1,60 @@
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
 
 function MiApi() {
-const [count, setCount] = useState(0)
-const [dia, setDia] = useState({})
+  const [dataUrl1, setDataUrl1] = useState([]);
+  const [dataUrl2, setDataUrl2] = useState([]);
+  const [currentData, setCurrentData] = useState([]);
 
-const getDay = async () => {
-  const resp = await fetch('https://api.victorsanmartin.com/feriados/en.json')
-  const data = await resp.json()
-  setDia(data)
-}
+  const getData = async (url) => {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  };
 
-useEffect(() => {
-  getDay()
-},[])
+  useEffect(() => {
+    getData("https://api.victorsanmartin.com/feriados/en.json").then((data) => {
+      setDataUrl1(data?.data);
+    });
+  }, []);
+
+  const handleFirstButtonClick = () => {
+    setCurrentData(dataUrl1);
+  };
+
+  const handleSecondButtonClick = () => {
+    if (dataUrl2.length === 0) {
+      getData("https://www.feriadosapp.com/api/laws.json").then((data) => {
+        setDataUrl2(data?.data);
+        setCurrentData(data?.data);
+      });
+    } else {
+      setCurrentData(dataUrl2);
+    }
+  };
+
   return (
     <>
       <div>
-        <p>Estado es: {count}</p>
-        <button onClick={ () => setCount(count + 1)}>+1</button>
+        <button onClick={handleFirstButtonClick}>Feriados 2024</button>
+        <button onClick={handleSecondButtonClick}>
+          Leyes correspondientes a dias feriados
+        </button>
+        {currentData.map((item, index) => (
+          <div key={item.id ? item.id + "-" + index : index}>
+            {item.id && <p>Id: {item.id}</p>}
+            {item.title && <p>Titulo: {item.title}</p>}
+            {item.content && <p>Contenido: {item.content}</p>}
+            {item.link && <p>Titulo: {item.link}</p>}
+
+
+            {item.date && <p>Fecha: {item.date}</p>}
+            {item.extra && <p>Extra: {item.extra}</p>}
+            {item.type && <p>Tipo: {item.type}</p>}
+          </div>
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default MiApi
+export default MiApi;
